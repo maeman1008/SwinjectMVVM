9b0a00397cb5c53912a29dd121990d4b2f5a0845
Ryoto Maeda <ryoto.maeda@icloud.com>
2016-04-27 23:04:44 +0900

    add response entity

---
+++ Model/Entity/ResponseEntity.swift
@@ -0,0 +1,22 @@
+//
+//  ResponseEntity.swift
+//  SwinjectMVVM
+//
+//  Created by MaedaRyoto on 2016/04/27.
+//  Copyright © 2016年 ika. All rights reserved.
+//
+import Himotoki
+
+public struct ResponseEntity {
+    public let totalCount: Int64
+    public let images: [ImageEntity]
+}
+
+extension ResponseEntity: Decodable {
+    public static func decode(e: Extractor) throws -> ResponseEntity {
+        return try ResponseEntity(
+            totalCount: e <| "totalHits",
+            images: e <|| "hits"
+        )
+    }
+}
