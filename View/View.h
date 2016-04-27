9cb9b67fd9573dea29ad49956d6e917563518839
Ryoto Maeda <ryoto.maeda@icloud.com>
2016-04-27 23:00:04 +0900

    add project settings

---
+++ View/View.h
@@ -0,0 +1,19 @@
+//
+//  View.h
+//  View
+//
+//  Created by MaedaRyoto on 2016/04/27.
+//  Copyright © 2016年 ika. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for View.
+FOUNDATION_EXPORT double ViewVersionNumber;
+
+//! Project version string for View.
+FOUNDATION_EXPORT const unsigned char ViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <View/PublicHeader.h>
+
+
---
+++ ViewModel/ViewModel.h
@@ -0,0 +1,19 @@
+//
+//  ViewModel.h
+//  ViewModel
+//
+//  Created by MaedaRyoto on 2016/04/27.
+//  Copyright © 2016年 ika. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ViewModel.
+FOUNDATION_EXPORT double ViewModelVersionNumber;
+
+//! Project version string for ViewModel.
+FOUNDATION_EXPORT const unsigned char ViewModelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ViewModel/PublicHeader.h>
+
+
